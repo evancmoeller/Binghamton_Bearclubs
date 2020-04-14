cc5592a0d839624a2f2d994884e56f3015c6577a
Ubuntu <ubuntu@ip-172-31-44-124.us-east-2.compute.internal>
2020-04-14 00:27:45 +0000

    Fixing stuff

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,4 +1,25 @@
 class ClubsController < ApplicationController
+  def club_params
+    params.require(:club).permit(:name, :description)
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    @club = Club.create!(club_params)
+    flash[:notice] = "'#{@club.name}' was created."
+    redirect_to clubs_path
+  end
+  
+  def destroy
+    @club = Club.find params[:id]
+    @club.destroy
+    flash[:notice] = "'#{@club.name}' was deleted."
+    redirect_to clubs_path
+  end
+  
   def temp
   
   end
--- spec/controllers/clubs_controller_spec.rb
+++ spec/controllers/clubs_controller_spec.rb
@@ -1,12 +1,49 @@
 require 'rails_helper'
 
-describe ClubsController do
-  describe 'create a new club', :type => :controller do
-      let(:params) {{name: 'test', description: 'blah, blah, blah...', admins: 'none'}} 
-      let(:club1) {double('Club', params)}
+describe ClubsController, type: :controller do
+  describe '#create' do
+      let(:params) {{name: 'test'}} 
+      let(:club) {instance_double('Club', params)}
       it 'creates the new club' do
-          expect(Club).to receive(:create!).with(params).and_return(club1)
-          post :create, club1: params
+          expect(Club).to receive(:create!).with(params).and_return(club)
+          post :create, club: params
+      end
+      it 'sends a flash message' do
+          allow(Club).to receive(:create!).with(params).and_return(club)
+          post :create, club: params
+          expect(flash[:notice]).to match(/^\'[^']*\' was created.$/) 
+      end
+      it 'redirect_to clubs index' do
+          allow(Club).to receive(:create!).with(params).and_return(club)
+          post :create, club: params
+          expect(response).to redirect_to clubs_path
+      end
+  end
+  
+  describe '#destroy' do
+      let(:id1) {"1"}
+      let(:club) {instance_double('Club', name: "temp")}
+      it 'retrieves the club' do
+         expect(Club).to receive(:find).with(id1).and_return(club)
+         allow(club).to receive(:destroy)
+         delete :destroy, :id => id1
+      end
+      it 'calls the destroy method' do
+         allow(Club).to receive(:find).with(id1).and_return(club)
+         expect(club).to receive(:destroy)
+         delete :destroy, :id => id1
+      end
+      it 'sends a flash message' do
+         allow(Club).to receive(:find).with(id1).and_return(club)
+         allow(club).to receive(:destroy)
+         delete :destroy, :id => id1
+         expect(flash[:notice]).to match(/^\'[^']*\' was deleted.$/)
+      end
+      it 'redirect_to clubs index' do
+         allow(Club).to receive(:find).with(id1).and_return(club)
+         allow(club).to receive(:destroy)
+         delete :destroy, :id => id1
+         expect(response).to redirect_to clubs_path
       end
   end
 end
