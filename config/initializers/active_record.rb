878408db70fd133aa4430338d76d3552599d71f8
Ubuntu <ubuntu@ip-172-31-44-124.us-east-2.compute.internal>
2020-05-10 22:04:54 +0000

    heroku

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -1,1 +1,1 @@
-ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:date_time] = {name: "timestamp without time zone"}
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:datetime] = {name: "timestamp without time zone"}
